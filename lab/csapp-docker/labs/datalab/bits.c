76e008fc7724a84029de02505f97302262d48f79
mika <mikasa2511592510@gmail.com>
2024-10-22 10:32:03 +0800

    ```实现allOddBits和negate函数的正确逻辑
    - allOddBits函数现在使用位操作来检查所有位是否为奇数- negate函数现在使用位反转和加一来计算负数
    ```

--- lab/csapp-docker/labs/datalab/bits.c
+++ lab/csapp-docker/labs/datalab/bits.c
@@ -183,7 +183,7 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+  return !(x + (x >> 1) + 1) ;
 }
 /* 
  * negate - return -x 
@@ -193,7 +193,7 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  return ~x + 1;
 }
 //3
 /* 
